1f2c146dd1f160eacbed8bc0cda0e794de1cbe84
Joshua <joshuagutierrez724@gmail.com>
2023-01-16 12:16:01 -0500

    Adding Spinner drop down menu and all the logic for empty fields.

--- app/src/main/java/com/example/androidexam/MainActivity.java
+++ app/src/main/java/com/example/androidexam/MainActivity.java
@@ -5,16 +5,27 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button registerBtn;
     private ConstraintLayout constraintLay;
     private EditText name, email, password;
+    private CheckBox license;
+    private RadioGroup gender;
+    private Spinner country;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
          email = findViewById(R.id.edtxtEmail);
          password = findViewById(R.id.edtxtPassword);
 
+         license = findViewById(R.id.checkBox);
+         gender = findViewById(R.id.genderToggle);
+         country = findViewById(R.id.country);
+
+        ArrayList<String> countries = new ArrayList<>();
+        countries.add("United Sates");
+        countries.add("Germany");
+        countries.add("Other");
+
+        ArrayAdapter<String> countriesAdpater = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                countries
+        );
+
+        country.setAdapter(countriesAdpater);
 
     }
 
@@ -38,13 +65,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.button2:
-                if()
-                showSnackbar();
-            break;
+                //checks if there's a entry missing
+                //TextUtils is a set of utility functions on String objects
+                if(!TextUtils.isEmpty(name.getText())
+                    && !TextUtils.isEmpty(email.getText())
+                    && !TextUtils.isEmpty(password.getText())
+                    && license.isChecked() && radioCheck()){
+
+                    showSnackbar();
+                    break;
+                }
+                else
+                    showEmpty();
+                    break;
+
         }
     }
 
     public void showSnackbar(){
-        Snackbar.make(constraintLay, , Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(constraintLay,getString(R.string.Registration), Snackbar.LENGTH_SHORT).show();
+    }
+    public void showEmpty(){
+        Snackbar.make(constraintLay, getString(R.string.Empty), Snackbar.LENGTH_SHORT).show();
+    }
+
+    public boolean radioCheck(){
+        if(gender.getCheckedRadioButtonId() ==-1){
+            return false;
+        }
+        else
+            return true;
     }
 }
